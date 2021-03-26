5a069cf41b8fdfd9ce8bbc235ac2fe4d8b3c91b0
Jairo Doni <jairo.doni97@gmail.com>
2021-03-26 18:27:52 -0300

    Base do projeto em next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+//para tipar o Component e pageProps use o AppProps
+import { AppProps } from 'next/app'
+
+import  '../styles/global.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
