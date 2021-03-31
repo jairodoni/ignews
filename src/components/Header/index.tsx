cfd1685b010c86a12546a95fea760b7822105858
Jairo Doni <jairo.doni97@gmail.com>
2021-03-30 22:27:10 -0300

    Prismic CMS  integrado ao ignews

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
-import { SignInButton } from '../SingnInButton'
-import styles from './styles.module.scss'
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { SignInButton } from '../SingnInButton';
+import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
+
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          {/* prefetch deixa os posts pré-carregados */}
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
@@ -16,3 +27,4 @@ export function Header() {
     </header>
   )
 }
+// className={asPath === '/' ? styles.active : ''}
